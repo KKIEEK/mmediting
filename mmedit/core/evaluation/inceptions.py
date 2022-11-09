dd9ebd2835493446e6e80b58c95b96c099e7ff04
Junhwa Song <ethan9867@si-analytics.ai>
2022-11-09 09:34:14 +0000

    Prototyping InceptionV3 module for StyleGAN

--- mmedit/core/evaluation/inceptions.py
+++ mmedit/core/evaluation/inceptions.py
@@ -16,7 +16,10 @@ class InceptionV3:
     """
 
     def __init__(self, device='cpu', **inception_kwargs):
-        self.inception = _InceptionV3(**inception_kwargs).to(device)
+        # self.inception = _InceptionV3(**inception_kwargs).to(device)
+        self.inception = _load_inception_from_url(
+            'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metrics/inception-2015-12-05.pt'
+        ).to(device).eval()
         self.device = device
 
     def __call__(self, img1, img2, crop_border=0):
@@ -37,12 +40,15 @@ class InceptionV3:
     def img2tensor(self, img):
         img = img.transpose((2, 0, 1))
         img = np.expand_dims(img, axis=0)
-        return torch.from_numpy(img / 255.).to(
-            device=self.device, dtype=torch.float32)
+        #return torch.from_numpy(img / 255.).to(
+        #    device=self.device, dtype=torch.float32)
+        return torch.tensor(img).to(device=self.device, dtype=torch.uint8)
 
     def forward_inception(self, x):
-        with torch.no_grad():
-            return self.inception(x)[0].view(x.shape[0], -1).cpu()
+        #with torch.no_grad():
+        with disable_gpu_fuser_on_pt19():
+            return self.inception(
+                x, return_features=True).cpu()  #[0].view(x.shape[0], -1).cpu()
 
 
 def frechet_distance(X, Y, eps=1e-6):
@@ -137,3 +143,154 @@ class KID:
             kid.append(mmd2(X_, Y_, biased=self.biased))
         kid = np.array(kid)
         return dict(KID_MEAN=kid.mean(), KID_STD=kid.std())
+
+
+import hashlib
+import os
+
+import click
+import requests
+import torch.distributed as dist
+import torch.nn as nn
+from requests.exceptions import InvalidURL, RequestException, Timeout
+
+MMEDIT_CACHE_DIR = os.path.expanduser('~') + '/.cache/openmmlab/mmedit/'
+
+
+def get_content_from_url(url, timeout=15, stream=False):
+    """Get content from url.
+
+    Args:
+        url (str): Url for getting content.
+        timeout (int): Set the socket timeout. Default: 15.
+    """
+    try:
+        response = requests.get(url, timeout=timeout, stream=stream)
+    except InvalidURL as err:
+        raise err  # type: ignore
+    except Timeout as err:
+        raise err  # type: ignore
+    except RequestException as err:
+        raise err  # type: ignore
+    except Exception as err:
+        raise err  # type: ignore
+    return response
+
+
+def download_from_url(url,
+                      dest_path=None,
+                      dest_dir=MMEDIT_CACHE_DIR,
+                      hash_prefix=None):
+    """Download object at the given URL to a local path.
+
+    Args:
+        url (str): URL of the object to download.
+        dest_path (str): Path where object will be saved.
+        dest_dir (str): The directory of the destination. Defaults to
+            ``'~/.cache/openmmlab/mmgen/'``.
+        hash_prefix (string, optional): If not None, the SHA256 downloaded
+            file should start with `hash_prefix`. Default: None.
+    Return:
+        str: path for the downloaded file.
+    """
+    # get the exact destination path
+    if dest_path is None:
+        filename = url.split('/')[-1]
+        dest_path = os.path.join(dest_dir, filename)
+
+    if dest_path.startswith('~'):
+        dest_path = os.path.expanduser('~') + dest_path[1:]
+
+    # advoid downloading existed file
+    if os.path.exists(dest_path):
+        return dest_path
+
+    rank, ws = 0, 1
+
+    # only download from the master process
+    if rank == 0:
+        # mkdir
+        _dir = os.path.dirname(dest_path)
+        os.makedirs(_dir, exist_ok=True)
+
+        if hash_prefix is not None:
+            sha256 = hashlib.sha256()
+
+        response = get_content_from_url(url, stream=True)
+        size = int(response.headers.get('content-length'))
+        with open(dest_path, 'wb') as fw:
+            content_iter = response.iter_content(chunk_size=1024)
+            with click.progressbar(content_iter, length=size / 1024) as chunks:
+                for chunk in chunks:
+                    if chunk:
+                        fw.write(chunk)
+                        fw.flush()
+                        if hash_prefix is not None:
+                            sha256.update(chunk)
+
+        if hash_prefix is not None:
+            digest = sha256.hexdigest()
+            if digest[:len(hash_prefix)] != hash_prefix:
+                raise RuntimeError(
+                    f'invalid hash value, expected "{hash_prefix}", but got '
+                    f'"{digest}"')
+
+    # sync the other processes
+    if ws > 1:
+        dist.barrier()
+
+    return dest_path
+
+
+def _load_inception_from_path(inception_path):
+    """Load inception from passed path.
+
+    Args:
+        inception_path (str): The path of inception.
+    Returns:
+        nn.Module: The loaded inception.
+    """
+    print('Try to load Tero\'s Inception Model from '
+          f'\'{inception_path}\'.', 'current')
+    try:
+        model = torch.jit.load(inception_path)
+        print('Load Tero\'s Inception Model successfully.', 'current')
+    except Exception as e:
+        model = None
+        print('Load Tero\'s Inception Model failed. '
+              f'\'{e}\' occurs.', 'current')
+    return model
+
+
+def _load_inception_from_url(inception_url: str) -> nn.Module:
+    """Load Inception network from the give `inception_url`"""
+    inception_url = inception_url if inception_url else TERO_INCEPTION_URL
+    print(f'Try to download Inception Model from {inception_url}...',
+          'current')
+    try:
+        path = download_from_url(inception_url, dest_dir=MMEDIT_CACHE_DIR)
+        print('Download Finished.', 'current')
+        return _load_inception_from_path(path)
+    except Exception as e:
+        print(f'Download Failed. {e} occurs.', 'current')
+        return None
+
+
+from contextlib import contextmanager
+
+
+@contextmanager
+def disable_gpu_fuser_on_pt19():
+    """On PyTorch 1.9 a CUDA fuser bug prevents the Inception JIT model to run.
+
+    Refers to:
+      https://github.com/GaParmar/clean-fid/blob/5e1e84cdea9654b9ac7189306dfa4057ea2213d8/cleanfid/inception_torchscript.py#L9  # noqa
+      https://github.com/GaParmar/clean-fid/issues/5
+      https://github.com/pytorch/pytorch/issues/64062
+    """
+    if torch.__version__.startswith('1.9.'):
+        old_val = torch._C._jit_can_fuse_on_gpu()
+        torch._C._jit_override_can_fuse_on_gpu(False)
+    yield
+    if torch.__version__.startswith('1.9.'):
+        torch._C._jit_override_can_fuse_on_gpu(old_val)
--- tests/test_metrics/test_inceptions.py
+++ tests/test_metrics/test_inceptions.py
@@ -1,1 +1,41 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import mmcv
+import numpy as np
+import pytest
+
+from mmedit.core.evaluation.metrics import (
+    InceptionV3, FID, KID)
+
+
+def test_inception():
+    model = InceptionV3()
+    feats = model(np.random.randint(0, 256, (224, 224, 3)))
+    assert len(feats) == 2
+    assert feats[0].shape == (1, 2048)
+    assert feats[1].shape == (1, 2048)
+
+
+def test_fid():
+    model = InceptionV3()
+    feats = [
+        model(np.random.randint(0, 256, (224, 224, 3)))
+        for _ in range(10)
+    ]
+    X, Y = zip(*feats)
+    X, Y = np.array(X), np.array(Y)
+
+    fid = FID()
+    assert np.allclose()
+
+
+def test_kid():
+    model = InceptionV3()
+    feats = [
+        model(np.random.randint(0, 256, (224, 224, 3)))
+        for _ in range(10)
+    ]
+    X, Y = zip(*feats)
+    X, Y = np.array(X), np.array(Y)
+
+    kid = KID()
+    assert np.allclose()
