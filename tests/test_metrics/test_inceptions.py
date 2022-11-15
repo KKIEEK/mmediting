42b0d11af77f39cbe1189b9ccef41a7a1a904671
Junhwa Song <ethan9867@si-analytics.ai>
2022-11-15 09:42:35 +0000

    Fix

--- tests/test_metrics/test_inceptions.py
+++ tests/test_metrics/test_inceptions.py
@@ -3,31 +3,40 @@ import numpy as np
 import pytest
 import torch
 
-from mmedit.core.evaluation.metrics import FID, KID, InceptionV3
+from mmedit.core.evaluation.inception_utils import (PyTorchInceptionV3,
+                                                    StyleGANInceptionV3)
+from mmedit.core.evaluation.inceptions import FID, KID, InceptionV3
 
 
 def test_inception():
     img1 = np.random.randint(0, 256, (224, 224, 3))
     img2 = np.random.randint(0, 256, (224, 224, 3))
 
-    # test `img2tensor` and `forward_inception` method
-    # for StyleGAN style's inception
+    # for StyleGAN style inception
     inception = InceptionV3(style='StyleGAN')
+    assert isinstance(inception.inception, StyleGANInceptionV3)
+
     t = inception.img2tensor(img1)
     assert isinstance(t, torch.Tensor)
     assert t.dtype == torch.uint8
     assert t.shape == (1, 3, 224, 224)
+
     t = inception.forward_inception(t)
+    assert t.device == torch.device('cpu')
     assert t.shape == (1, 2048)
 
-    # test `img2tensor` and `forward_inception` method for PyTorch style's one
+    # for PyTorch style inception
     inception = InceptionV3(style=None)
+    assert isinstance(inception.inception, PyTorchInceptionV3)
+
     t = inception.img2tensor(img1)
     assert isinstance(t, torch.Tensor)
     assert t.dtype == torch.float32
     assert t.shape == (1, 3, 224, 224)
-    assert 0 <= t <= 1
+    assert np.logical_and(0 <= t, t <= 1).all()
+
     t = inception.forward_inception(t)
+    assert t.device == torch.device('cpu')
     assert t.shape == (1, 2048)
 
     # test `__call__` method in cpu
@@ -50,7 +59,7 @@ def test_fid():
     fid = FID()
     fid_result = fid(np.ones((10, 2048)), np.ones((10, 2048)))
     assert isinstance(fid_result, float)
-    assert np.testing.assert_equal(fid_result, 0)
+    assert fid_result == 0.0
 
 
 def test_kid():
@@ -58,8 +67,8 @@ def test_kid():
     kid_result = kid(np.ones((10, 2048)), np.ones((10, 2048)))
     assert isinstance(kid_result, dict)
     assert 'KID_MEAN' in kid_result and 'KID_STD' in kid_result
-    assert np.testing.assert_equal(kid_result['KID_MEAN'], 0)
-    assert np.testing.assert_equal(kid_result['KID_STD'], 0)
+    assert kid_result['KID_MEAN'] == 0.0
+    assert kid_result['KID_STD'] == 0.0
 
     # if sample size > number of samples
     with pytest.raises(ValueError):
